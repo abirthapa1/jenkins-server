0f9261b29be483e52f823542153d43e4ae1ccc96
abirthapa1 <abirthapa1@gmail.com>
2026-04-14 13:46:26 -0400

    changed the names for the module and added alb and bastion as well. With updated outputs as well

--- main.tf
+++ main.tf
@@ -13,6 +13,9 @@ provider "aws" {
   region = var.region
 }
 
+#####################
+#### VPC DEFINED ####
+####################
 ## VALUES FOR VPC MODULE
 module "network_vpc" {
   source = "./terraform_repo/network/vpc"
@@ -20,78 +23,200 @@ module "network_vpc" {
   vpc_name = "jenkins-server"
   cidr     = "10.0.0.0/16"
 
-  azs = ["us-east-2a"]
-
-  public_subnets  = ["10.0.1.0/24"]
-  private_subnets = ["10.0.11.0/24"]
+  azs             = ["us-east-2a", "us-east-2b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
 
   vpc_tags = {
     Environment = "dev"
     Project     = "jenkins"
   }
 
+  enable_nat_gateway = true
+
 }
 
+#########################
+#### SECURITY GROUP ####
+#######################
 ## VALUES FOR SECURITY GROUP
-module "network_sg" {
-  source = "./terraform_repo/network/security_group"
 
-  sg_name = "jenkins-server-sg"
-  vpc_id  = module.network_vpc.vpc_id
+### BASTION
+module "bastion_sg" {
+  source  = "./terraform_repo/network/security_group"
+  sg_name = "bastion_sg"
 
-  ingress_cidr_blocks      = ["0.0.0.0/0"]
-  ingress_rules            = ["http-80-tcp", "ssh-tcp"]
+  vpc_id = module.network_vpc.vpc_id
+
+  ingress_with_cidr_blocks = [{
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = "0.0.0.0/0"
+    description = "SSH for admins"
+  }]
+
+  egress_with_cidr_blocks = []
+
+  egress_with_source_security_group_id = [{
+    from_port                = 22
+    to_port                  = 22
+    protocol                 = "tcp"
+    source_security_group_id = module.jenkins_master_sg.sg_id
+    cidr_blocks              = "0.0.0.0/0"
+    description              = "SSH to Jenkins master only"
+  }]
+
+}
+
+### JENKINS MASTER SG
+module "jenkins_master_sg" {
+  source  = "./terraform_repo/network/security_group"
+  sg_name = "jenkins_master_sg"
+
+  vpc_id = module.network_vpc.vpc_id
+
   ingress_with_cidr_blocks = []
 
-  egress_with_cidr_blocks = [{
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = "0.0.0.0/0"
+  ingress_with_source_security_group_id = [{
+    from_port                = 22
+    to_port                  = 22
+    protocol                 = "tcp"
+    source_security_group_id = module.bastion_sg.sg_id
+    description              = "SSH from bastion only"
+    }, {
+    from_port                = 8080
+    to_port                  = 8080
+    protocol                 = "tcp"
+    source_security_group_id = module.alb_sg.sg_id
+    description              = "Web GUI from ALB"
   }]
 
-  sg_tags = {
-    Environment = "dev"
-    Name        = "jenkins-server-sg"
-  }
+  egress_with_cidr_blocks = [{
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = "0.0.0.0/0"
+    description = "outbound Http for plugins and git"
+  }]
+
+
+
+  egress_with_source_security_group_id = [{
+    from_port                = 22
+    to_port                  = 22
+    protocol                 = "tcp"
+    source_security_group_id = module.jenkins_agent_sg.sg_id
+  }]
+
 }
 
-module "master_sg" {
-  source = "./terraform_repo/network/security_group"
+### JENKIN AGENT SG
+module "jenkins_agent_sg" {
+  source  = "./terraform_repo/network/security_group"
+  sg_name = "jenkins_agent_sg"
 
-  sg_name = "jenkins-master-sg"
-  vpc_id  = module.network_vpc.vpc_id
+  vpc_id = module.network_vpc.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = []
+  ingress_with_cidr_blocks = []
+  ingress_with_source_security_group_id = [{
+    from_port                = 22
+    to_port                  = 22
+    protocol                 = "tcp"
+    source_security_group_id = module.jenkins_master_sg.sg_id
+    description              = "SSH from Jenkins master"
+  }]
+  egress_with_cidr_blocks = [{
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = "0.0.0.0/0"
+    description = "Outbound http for builds git, artifacts"
+  }]
+}
+
+
+### ALB SG
+module "alb_sg" {
+  source  = "./terraform_repo/network/security_group"
+  sg_name = "alb_sg"
+
+  vpc_id = module.network_vpc.vpc_id
+
   ingress_with_cidr_blocks = [{
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    description = "allow traffic from tcp/8080"
     cidr_blocks = "0.0.0.0/0"
+    description = "Accessing the Alb"
   }]
 
-  egress_with_cidr_blocks = [{
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = "0.0.0.0/0"
+  egress_with_cidr_blocks = []
+
+  egress_with_source_security_group_id = [{
+    from_port                = 8080
+    to_port                  = 8080
+    protocol                 = "tcp"
+    source_security_group_id = module.jenkins_master_sg.sg_id
   }]
 
-  sg_tags = {
-    Environment = "dev"
-    Name        = "jenkins-master-sg"
-  }
 }
 
-## VALUES FOR EC2 INSTANCE
+
+
+#####################
+#### ALB DEFINED ####
+#####################
+
+### ALB
+module "jenkins_ALB" {
+  source                = "./terraform_repo/network/load_balancer"
+  target_group_name     = "jenkins"
+  target_group_port     = 8080
+  target_group_protocol = "HTTP"
+  vpc_id                = module.network_vpc.vpc_id
+
+  # target_control_port = 8080
+  health_check_path = "/login"
+
+  alb_name           = "jenkins-alb"
+  scheme             = false
+  load_balancer_type = "application"
+  alb_security_grp   = [module.alb_sg.sg_id]
+  subnets            = module.network_vpc.public_subnets
+
+}
+
+############################
+#### INSTANCES DEFINED ####
+##########################
+
+## VALUES FOR BASTION
+module "bastion_ec2" {
+  source        = "./terraform_repo/infra"
+  instance_name = "bastion-host"
+
+  instance_type     = "t3.micro"
+  ami_ssm_parameter = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  security_group    = [module.bastion_sg.sg_id]
+  subnet_id         = module.network_vpc.public_subnets[0]
+  key_name          = aws_key_pair.aws-key.key_name
+
+  associate_public_ip_address = true
+  root_block_device = {
+    size = var.volume_size
+    type = "gp3"
+  }
+
+}
+
+## VLAUES FOR MASTER AND AGENTS
 module "infra_ec2" {
   source = "./terraform_repo/infra"
 
   for_each = toset(["master-node", "java-node", "python-node"])
 
-  jenkins_main_name = "jenkins-${each.key}"
+  instance_name = "jenkins-${each.key}"
 
   #jenkins_main_name = "jenkins-master"
   instance_type     = "t3.micro"
@@ -99,9 +224,13 @@ module "infra_ec2" {
 
   ### Adding the master security group only to the master node
   ### so that we can access the jenkins website on port 8080
-  security_group = concat([module.network_sg.sg_id], (each.key == "master-node" ? [module.master_sg.sg_id] : []))
+  security_group = concat([module.jenkins_agent_sg.sg_id], (each.key == "master-node" ? [module.jenkins_master_sg.sg_id] : []))
   key_name       = aws_key_pair.aws-key.key_name
-  subnet_id      = module.network_vpc.public_subnets[0]
+  subnet_id      = module.network_vpc.private_subnets[0]
+
+  associate_public_ip_address = false
+
+
 
   root_block_device = {
     size = var.volume_size
--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,89 @@
 ## VPC outputs
 output "vpc_id" {
-  value = module.network_vpc.vpc_id
+  description = "Outputs VPCs id"
+  value       = module.network_vpc.vpc_id
 }
 
-output "vpc_subnet" {
-  value = module.network_vpc.public_subnets
+output "vpc_public_subnets" {
+  description = "Outputs the public subnets attached in VPC"
+  value       = module.network_vpc.public_subnets
 }
 
-## Security Group outputs
-output "sg_id" {
-  value = module.network_sg.sg_id
+output "vpc_private_subnets" {
+  description = "Outputs the private subnets attached in VPC"
+  value       = module.network_vpc.private_subnets
 }
 
-output "sg_arn" {
-  value = module.network_sg.sg_arn
+###############
+## SG OUTPUTS
+###############
+output "jenkins_master_sg_id" {
+  description = "Outputs the jenkins master SG id"
+  value       = module.jenkins_master_sg.sg_id
 }
 
-output "ec2_public_ip" {
-  # value = module.infra_ec2.public_ip
-  value = {
-    for name, mod in module.infra_ec2 :
-    name => mod.public_ip
-  }
+output "jenkins_agent_sg_id" {
+  description = "Outputs the jenkins agents SG id"
+  value       = module.jenkins_agent_sg.sg_id
 }
 
-output "master_id" {
+output "alb_sg_id" {
+  description = "Outputs the ALB SG id"
+  value       = module.alb_sg.sg_id
+}
+
+output "bastion_sg_id" {
+  description = "Outputs the bastion SG id"
+  value       = module.bastion_sg.sg_id
+}
+
+output "jenkins_master_sg_arn" {
+  description = "Outputs the jenkins master SG arn"
+  value       = module.jenkins_master_sg.sg_arn
+}
+
+output "jenkins_agent_sg_arn" {
+  description = "Outputs the jenkins agents SG arn"
+  value       = module.jenkins_agent_sg.sg_arn
+}
+
+output "alb_sg_arn" {
+  description = "Outputs the ALB SG arn"
+  value       = module.alb_sg.sg_arn
+}
+
+output "bastion_sg_arn" {
+  description = "Outputs the bastion SG arn"
+  value       = module.bastion_sg.sg_arn
+}
+
+##############
+## ALB OUTPUTS
+##############
+output "alb_id" {
+  description = "Outputs the ALB id"
+  value       = module.jenkins_ALB.alb_id
+
+}
+output "alb_arn" {
+  value = module.jenkins_ALB.alb_arn
+}
+
+###############
+## EC2 OUTPUTS
+###############
+# output "ec2_public_ip" {
+#   # value = module.infra_ec2.public_ip
+#   value = {
+#     for name, mod in module.infra_ec2 :
+#     name => mod.public_ip
+#   }
+# }
+
+output "ec2_instance_id" {
   # value = module.infra_ec2.master_id
   value = {
     for name, mod in module.infra_ec2 :
-    name => mod.master_id
+    name => mod.ec2_instance_id
   }
 }
